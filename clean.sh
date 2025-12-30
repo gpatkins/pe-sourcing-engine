5cc30017520a2eac23f6c2a7e66b3963b182be59
Gabriel Atkinson <gabeatks@outlook.com>
2025-12-29 21:01:28 -0500

    v5.8: Remove all Docker files - rebuilding from scratch

--- clean.sh
+++ clean.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-echo "--- 🛑 Restarting App Container (Kills Zombies) ---"
-sudo docker compose restart app
-
-echo "\n--- 🧹 Vacuuming Database ---"
-sudo docker compose exec db psql -U pe_sourcer -d pe_sourcing_db -c "VACUUM FULL;"
-
 echo "\n--- 📉 Dropping System Cache ---"
 sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches'
 
